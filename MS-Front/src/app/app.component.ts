99a17b48220bd7d8e03a7be2bfce005e380b95d2
Frank Zhang <fz2907@vt.edu>
2022-07-15 00:08:46 -0400

    Try to make connection to local server

--- MS-Front/src/app/app.component.ts
+++ MS-Front/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'MS-Front';
+  constructor(private http: HttpClient) {  }
+
+  click() {
+   // @ts-ignore
+    return this.http.get('http://localhost:8080/api/v1/customers').subscribe(
+      (response) => {                           //Next callback
+        console.log('response received')
+        console.log(response);
+      },
+      (error) => {                              //Error callback
+        console.error('Request failed with error')
+        alert(error);
+      },
+      () => {                                   //Complete callback
+        console.log('Request completed')
+      })
+
+  }
+
+  ngOnInit(): void {
+  }
 }
